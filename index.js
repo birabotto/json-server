04eaa15afad12707a3089688ef7158c22b19bc0b
Bira Botto <tibira.botto@gmail.com>
2024-08-16 13:35:37 -0400

    change node runtime

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 // api/index.js
 const express = require("express");
 const jsonServer = require("json-server");
-const path = require("path");
+
 const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Set up json-server to use a custom db.json file
-const router = jsonServer.router({
-  ...JSON.parse(fs.readFileSync(path.join(__dirname, "db.json"))),
-});
-const middlewares = jsonServer.defaults();
-
-// Use json-server middlewares
-app.use(middlewares);
-app.use("/api", router);
+app.use("/api", jsonServer.router("db.json"));
 
 // Add additional routes for Express if needed
 app.get("/", (req, res) => {
