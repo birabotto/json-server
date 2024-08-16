9b43473c00e569db40296b842a0a1816b77f1124
Bira Botto <tibira.botto@gmail.com>
2024-08-16 13:54:01 -0400

    fix express

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel!!!"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
