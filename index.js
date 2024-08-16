9cf85a140873858fffd8c1df020b86414d2b2dad
Bira Botto <tibira.botto@gmail.com>
2024-08-16 13:52:19 -0400

    fix express

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 // api/index.js
+// const express = require("express");
+// const jsonServer = require("json-server");
+
+// const fs = require("fs");
+
+// const app = express();
+// const port = 3000process.env.PORT || ;
+
+// app.use("/api", jsonServer.router("db.json"));
+
+// // Add additional routes for Express if needed
+// app.get("/", (req, res) => {
+//   res.send("Hello from Express server with json-server!");
+// });
+
+// app.listen(port);
+
+// // Export the handler to be used by Vercel
+// module.exports = app;
+
 const express = require("express");
-const jsonServer = require("json-server");
-
-const fs = require("fs");
-
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use("/api", jsonServer.router("db.json"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Add additional routes for Express if needed
-app.get("/", (req, res) => {
-  res.send("Hello from Express server with json-server!");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.listen(port);
-
-// Export the handler to be used by Vercel
 module.exports = app;
